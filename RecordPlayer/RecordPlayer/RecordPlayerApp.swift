f9a5ca39e22e8b206091d66140edcbb4eb962284
Henri Keski-Sikkila <henri@keskisikkila.fi>
2022-02-14 18:55:17 +0200

    Add RecordPlayer demo

---
+++ RecordPlayer/RecordPlayer/RecordPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordPlayerApp.swift
+//  RecordPlayer
+//
+//  Created by Henri on 14.2.2022.
+//
+
+import SwiftUI
+
+@main
+struct RecordPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
