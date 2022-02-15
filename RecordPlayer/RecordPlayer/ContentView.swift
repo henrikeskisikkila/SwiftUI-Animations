3605136476d402aa40405a662cb79f781c1a7c0f
Henri Keski-Sikkila <henri@keskisikkila.fi>
2022-02-15 15:10:50 +0200

    Add RecordView

--- RecordPlayer/RecordPlayer/ContentView.swift
+++ RecordPlayer/RecordPlayer/ContentView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var shouldAnimate = false
+    @State private var degrees = 0.0
+    
     var body: some View {
-        RecordPlayerBox()
+        ZStack {
+            RecordPlayerBox()
+            RecordView(degrees: $degrees, shouldAnimate: $shouldAnimate)
+        }
     }
 }
 
---
+++ RecordPlayer/RecordPlayer/RecordView.swift
@@ -0,0 +1,29 @@
+//
+//  RecordView.swift
+//  RecordPlayer
+//
+//  Created by Henri on 15.2.2022.
+//
+
+import SwiftUI
+
+struct RecordView: View {
+    @Binding var degrees: Double
+    @Binding var shouldAnimate: Bool
+    
+    var body: some View {
+        Image("record")
+            .resizable()
+            .frame(width: 275, height: 275)
+            .rotationEffect(Angle.degrees(degrees))
+            .animation(
+                Animation.linear(duration: shouldAnimate ? 60 : 0).delay(1.5),
+                value: shouldAnimate)
+    }
+}
+
+struct RecordView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordView(degrees: .constant(2.0), shouldAnimate: .constant(true))
+    }
+}
