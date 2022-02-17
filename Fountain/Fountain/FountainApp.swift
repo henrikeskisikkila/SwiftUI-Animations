0de95d8f169e3c6efd43811cc204d79d2833d923
Henri Keski-Sikkila <henri@keskisikkila.fi>
2022-02-17 16:03:55 +0200

    Initial commit for fountain

---
+++ Fountain/Fountain/FountainApp.swift
@@ -0,0 +1,17 @@
+//
+//  FountainApp.swift
+//  Fountain
+//
+//  Created by Henri on 17.2.2022.
+//
+
+import SwiftUI
+
+@main
+struct FountainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
