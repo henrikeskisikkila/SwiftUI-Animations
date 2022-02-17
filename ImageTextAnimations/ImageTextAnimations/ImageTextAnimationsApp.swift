a22bdbf653d6e149789bfa6b3cd89d36ecc936d2
Henri Keski-Sikkila <henri@keskisikkila.fi>
2022-02-17 10:31:16 +0200

    Initial commit for ImageTextAnimations

---
+++ ImageTextAnimations/ImageTextAnimations/ImageTextAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageTextAnimationsApp.swift
+//  ImageTextAnimations
+//
+//  Created by Henri on 17.2.2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageTextAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
