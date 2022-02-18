d656e605f5605bf413fcf797241926af010ed7d4
Henri Keski-Sikkila <henri@keskisikkila.fi>
2022-02-18 18:55:59 +0200

    Initial commit for HueRotationBackgroundScroll

---
+++ HueRotationBackgroundScroll/HueRotationBackgroundScroll/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HueRotationBackgroundScroll
+//
+//  Created by Henri on 18.2.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HueRotationBackgroundScroll/HueRotationBackgroundScroll/HueRotationBackgroundScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  HueRotationBackgroundScrollApp.swift
+//  HueRotationBackgroundScroll
+//
+//  Created by Henri on 18.2.2022.
+//
+
+import SwiftUI
+
+@main
+struct HueRotationBackgroundScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
