1428b2f3ec81bc83a80441b6fd54c2604ef47e49
Henri Keski-Sikkila <henri@keskisikkila.fi>
2022-02-23 19:42:07 +0200

    Add Settings view

---
+++ PresentDismissAnimation/PresentDismissAnimation/SettingView.swift
@@ -0,0 +1,45 @@
+//
+//  SettingView.swift
+//  PresentDismissAnimation
+//
+//  Created by Henri on 23.2.2022.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    @State private var setDate = Date()
+    @State private var timezone = false
+    @State private var volume = 50.0
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("Date and Time")) {
+                    DatePicker(selection: $setDate, label: {
+                        Image(systemName: "calendar.circle")
+                    })
+                }
+                Section(header: Text("Time Zone")) {
+                    Toggle(isOn: $timezone, label: {
+                        HStack {
+                            Image(systemName: "timer")
+                            Text("Timezone")
+                        }
+                    })
+                }
+                Section(header: Text("Alarm Volume")) {
+                    Text("Volume \(String(format: "%.0f", volume as Double))")
+                    Slider(value: $volume, in: 0...100) { _ in }
+                }
+            }
+            .frame(width: 400, height: 625)
+        }
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
