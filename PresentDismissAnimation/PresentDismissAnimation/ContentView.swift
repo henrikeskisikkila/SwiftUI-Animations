53a8f14a5bbed5adc878aa8f7e23b5349d47a21f
Henri Keski-Sikkila <henri@keskisikkila.fi>
2022-02-23 18:49:59 +0200

    Initial commit for PresentDismissAnimation

---
+++ PresentDismissAnimation/PresentDismissAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PresentDismissAnimation
+//
+//  Created by Henri on 23.2.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PresentDismissAnimation/PresentDismissAnimation/PresentDismissAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentDismissAnimationApp.swift
+//  PresentDismissAnimation
+//
+//  Created by Henri on 23.2.2022.
+//
+
+import SwiftUI
+
+@main
+struct PresentDismissAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
