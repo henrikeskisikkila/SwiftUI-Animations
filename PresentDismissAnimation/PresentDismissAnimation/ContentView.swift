fa0f65d0e9d849db5c3ff6bb28b8ceb9dd6a9581
Henri Keski-Sikkila <henri@keskisikkila.fi>
2022-02-24 17:33:43 +0200

    Add GeometryEffect

--- PresentDismissAnimation/PresentDismissAnimation/ContentView.swift
+++ PresentDismissAnimation/PresentDismissAnimation/ContentView.swift
@@ -13,6 +13,23 @@ struct ContentView: View {
     }
 }
 
+struct PresentAndDismiss: GeometryEffect {
+    var offsetValue: Double
+    var animatableData: Double {
+        get {
+            offsetValue
+        }
+        set {
+            offsetValue = newValue
+        }
+    }
+    
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        <#code#>
+    }
+    
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
